014b4d8e4bb3df1bd1b7bc1363c6349d7808a3b8
pietrodev07 <pietro.dev.07@gmail.com>
2024-10-31 16:23:41 +0100

    refactor(app): use constanst for status codes

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,18 @@ import { Context } from "hono";
 import { routers } from "./routes";
 import { HTTPResponseError } from "hono/types";
 import { createRouter, configureOpenAPI } from "@/utils/openapi";
+import { OK, INTERNAL_SERVER_ERROR, NOT_FOUND } from "stoker/http-status-codes";
 
 export const baseHandler = (c: Context) => {
-  return c.json({ success: true, message: "Hello from elections maker api!" }, 200);
+  return c.json({ success: true, message: "Hello from elections maker api!" }, OK);
 };
 
 export const errorHandler = (_: Error | HTTPResponseError, c: Context) => {
-  return c.json({ success: false, message: "Internal Server Error!" }, 500);
+  return c.json({ success: false, message: "Internal Server Error!" }, INTERNAL_SERVER_ERROR);
 };
 
 export const notFoundHandler = (c: Context) => {
-  return c.json({ success: false, message: "Api endpoint not found!" }, 404);
+  return c.json({ success: false, message: "Api endpoint not found!" }, NOT_FOUND);
 };
 
 export const bootstrapApplication = () => {
